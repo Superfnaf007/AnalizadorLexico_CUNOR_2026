fe9f2ac570999673aed8c78ea70ef11b0ea3ff81
Superfnaf007 <manueldavidherrerareyes007@gmail.com>
2026-09-07 23:10:11 -0600

    Add files via upload

---
+++ Analizador Lexico/Analizador Lexico/Engine/AnalizadorLexico.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AnalizadorLexico.Models;
+
+namespace AnalizadorLexico.Engine
+{
+    public class AnalizadorLexico
+    {
+        private string codigoFuente;
+        public List<Token> TokensReconocidos { get; private set; }
+        public List<ErrorLexico> ErroresDetectados { get; private set; }
+        public TablaDeSimbolos TablaSimbolos { get; private set; }
+
+        public AnalizadorLexico(string codigoFuente)
+        {
+            this.codigoFuente = codigoFuente;
+            TokensReconocidos = new List<Token>();
+            ErroresDetectados = new List<ErrorLexico>();
+            TablaSimbolos = new TablaDeSimbolos();
+        }
+
+        public void Escanear()
+        {
+           
+        }
+    }
+}
